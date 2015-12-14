d72df7916eab996f05f40eefedf520dcfb7c8714
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-14 12:48:04 +0300

    Button connection added

--- js/buttonConnection.js
+++ js/buttonConnection.js
@@ -1,2 +1,13 @@
 var currencyChoiceContainer = document.getElementById('currency-choice-container');
-var currentCurrencyButon = document.getElementById('current-currency');
+var currentCurrencyButton = document.getElementById('current-currency');
+
+currencyChoiceContainer.onclick = function(e){
+  // var target = e.target;
+  if(usd.checked){
+    currentCurrencyButton.style.backgroundImage="url('img/usd_checked.svg')";
+  } else if(euro.checked){
+    currentCurrencyButton.style.backgroundImage="url('img/euro_checked.svg')";
+  } else if (rrubles.checked) {
+    currentCurrencyButton.style.backgroundImage="url('img/rruble_checked.svg')";
+  }
+};
