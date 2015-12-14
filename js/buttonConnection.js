1378125288f3501ddc751b28b736fcac1f01f9e8
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-14 17:26:33 +0300

    An appropriate algorithm is set into buttonConnection.js

--- js/buttonConnection.js
+++ js/buttonConnection.js
@@ -2,16 +2,24 @@ var currencyChoiceContainer = document.getElementById('currency-choice-container
 var currentCurrencyButton = document.getElementById('current-currency');
 
 currencyChoiceContainer.onclick = function(e){
-  // var target = e.target;
   currentCurrencyButton.classList.remove(currentCurrencyButton.dataset.currency);
-  if(usd.checked){
-    currentCurrencyButton.classList.add('usd');
-    currentCurrencyButton.dataset.currency="usd";
-  } else if(euro.checked){
-    currentCurrencyButton.classList.add('euro');
-    currentCurrencyButton.dataset.currency="euro";
-  } else if (rrubles.checked) {
-    currentCurrencyButton.classList.add('rruble');
-    currentCurrencyButton.dataset.currency="rruble";
-  }
+  var target = e.target;
+  // if(usd.checked){
+  //   currentCurrencyButton.classList.add('usd');
+  //   currentCurrencyButton.dataset.currency="usd";
+  // } else if(euro.checked){
+  //   currentCurrencyButton.classList.add('euro');
+  //   currentCurrencyButton.dataset.currency="euro";
+  // } else if (rrubles.checked) {
+  //   currentCurrencyButton.classList.add('rruble');
+  //   currentCurrencyButton.dataset.currency="rruble";
+  // }
+  while (target != this) {
+      if (target.tagName === 'LABEL') {
+        currentCurrencyButton.classList.add(target.dataset.setHidingClass);
+        currentCurrencyButton.dataset.currency=target.dataset.setHidingClass;
+        return;
+      }
+      target = target.parentNode;
+    }
 };
