3e6cbfdccbb7e31937d76ac14a3dd52aa9a09434
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-14 17:27:28 +0300

    All unnecessary comments are deleted from the buttonConnection.js

--- js/buttonConnection.js
+++ js/buttonConnection.js
@@ -4,16 +4,6 @@ var currentCurrencyButton = document.getElementById('current-currency');
 currencyChoiceContainer.onclick = function(e){
   currentCurrencyButton.classList.remove(currentCurrencyButton.dataset.currency);
   var target = e.target;
-  // if(usd.checked){
-  //   currentCurrencyButton.classList.add('usd');
-  //   currentCurrencyButton.dataset.currency="usd";
-  // } else if(euro.checked){
-  //   currentCurrencyButton.classList.add('euro');
-  //   currentCurrencyButton.dataset.currency="euro";
-  // } else if (rrubles.checked) {
-  //   currentCurrencyButton.classList.add('rruble');
-  //   currentCurrencyButton.dataset.currency="rruble";
-  // }
   while (target != this) {
       if (target.tagName === 'LABEL') {
         currentCurrencyButton.classList.add(target.dataset.setHidingClass);
