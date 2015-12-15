4c5d414dfb357b700a2d7bf6fcec96f582bb7a08
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-15 10:00:20 +0300

    Currency button background wrong behavior correction (background dussapeard while onclicking on free space of '.input-wrap')

--- js/buttonConnection.js
+++ js/buttonConnection.js
@@ -2,10 +2,10 @@ var currencyChoiceContainer = document.getElementById('currency-choice-container
 var currentCurrencyButton = document.getElementById('current-currency');
 
 currencyChoiceContainer.onclick = function(e){
-  currentCurrencyButton.classList.remove(currentCurrencyButton.dataset.currency);
   var target = e.target;
   while (target != this) {
       if (target.tagName === 'LABEL') {
+        currentCurrencyButton.classList.remove(currentCurrencyButton.dataset.currency);
         currentCurrencyButton.classList.add(target.dataset.setHidingClass);
         currentCurrencyButton.dataset.currency=target.dataset.setHidingClass;
         return;
