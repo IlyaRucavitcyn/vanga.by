3f96f00e1ec1f48608a562f8583faa049f3a209b
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-16 10:03:12 +0300

    buttonCnnection.js rewriting#1 : an old onclick function is commented

--- js/buttonConnection.js
+++ js/buttonConnection.js
@@ -2,14 +2,14 @@ var currencyChoiceContainer = document.getElementById('currency-choice-container
 var currentCurrencyButton = document.getElementById('current-currency');
 
 currencyChoiceContainer.onclick = function(e){
-  var target = e.target;
-  while (target != this) {
-      if (target.tagName === 'LABEL') {
-        currentCurrencyButton.classList.remove(currentCurrencyButton.dataset.currency);
-        currentCurrencyButton.classList.add(target.dataset.setHidingClass);
-        currentCurrencyButton.dataset.currency=target.dataset.setHidingClass;
-        return;
-      }
-      target = target.parentNode;
-    }
+  // var target = e.target;
+  // while (target != this) {
+  //     if (target.tagName === 'LABEL') {
+  //       currentCurrencyButton.classList.remove(currentCurrencyButton.dataset.currency);
+  //       currentCurrencyButton.classList.add(target.dataset.setHidingClass);
+  //       currentCurrencyButton.dataset.currency=target.dataset.setHidingClass;
+  //       return;
+  //     }
+  //     target = target.parentNode;
+  //   }
 };
