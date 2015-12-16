996169293db49386fc026dae6f8a66f323be8b66
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-16 11:40:43 +0300

    buttonConnection.js rewriting#2 : positioning .currency-choice-container bug fixing

--- js/buttonConnection.js
+++ js/buttonConnection.js
@@ -2,7 +2,7 @@ var currencyChoiceContainer = document.getElementById('currency-choice-container
 var currentCurrencyButton = document.getElementById('current-currency');
 
 currencyChoiceContainer.onclick = function(e){
-  // var target = e.target;
+   var target = e.target;
   // while (target != this) {
   //     if (target.tagName === 'LABEL') {
   //       currentCurrencyButton.classList.remove(currentCurrencyButton.dataset.currency);
