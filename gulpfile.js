af661447b35a4ff78a49f0fceca1b219dfe0e190
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-17 21:56:35 +0300

    Gulp & gulp csslint are reinstalled and included into devDepencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var csslint = require('gulp-csslint');
 
 gulp.task('css', function() {
-  gulp.src('css/style.css')
+  gulp.src('css/*.css')
     .pipe(csslint())
     .pipe(csslint.reporter());
 });
