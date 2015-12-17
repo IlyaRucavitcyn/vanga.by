e402c34e6a244652078a5f972240fa6179a74117
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-17 22:36:08 +0300

    NPM package creation again

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
+var gulp = require('gulp');
 var csslint = require('gulp-csslint');
 
 gulp.task('css', function() {
-  gulp.src('css/*.css')
+  gulp.src('css/style.css')
     .pipe(csslint())
     .pipe(csslint.reporter());
 });
