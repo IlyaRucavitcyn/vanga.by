2e769b8f98291f4a67945fc1b9ed864ff1bb0140
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-18 20:48:14 +0300

    Reset.css cleaned up in gulp csslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var csslint = require('gulp-csslint');
 
 gulp.task('css', function() {
-  gulp.src('css/style.css')
+  gulp.src('css/reset.css')
     .pipe(csslint())
     .pipe(csslint.reporter());
 });
