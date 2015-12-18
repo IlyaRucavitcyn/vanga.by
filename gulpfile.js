7fb60bd1064b9b9e6ceaf63a657fe9633f832fc3
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-18 22:09:58 +0300

    Rubbish ceaning : normalize.css is deleted

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var csslint = require('gulp-csslint');
 
 gulp.task('css', function() {
-  gulp.src('css/reset.css')
+  gulp.src('css/main.css')
     .pipe(csslint())
     .pipe(csslint.reporter());
 });
