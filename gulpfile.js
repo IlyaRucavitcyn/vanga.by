b4e4c08fa86eb94201c5a5318e896d36d706f677
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-18 22:50:44 +0300

    style.css modifying : fallback colors are set

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var csslint = require('gulp-csslint');
 
 gulp.task('css', function() {
-  gulp.src('css/main.css')
+  gulp.src('css/style.css')
     .pipe(csslint())
     .pipe(csslint.reporter());
 });
