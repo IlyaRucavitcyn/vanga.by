3cf8f9b7b399b05d67d1726a5a15bcda023c8d1f
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-21 12:55:24 +0300

    Gulp concat css installation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var csslint = require('gulp-csslint');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('css', function() {
-  gulp.src('css/*.css')
+  gulp.src('out/css/*.css')
     .pipe(csslint({
       'adjoining-classes':false
     }))
     .pipe(csslint.reporter());
+});
+
+gulp.task('concatcss', function () {
+  return gulp.src('css/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(gulp.dest('css/'));
 });
