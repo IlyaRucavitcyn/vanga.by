a2f001250b216397b3594acb04dc34cc473d4ef9
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2015-12-22 10:52:37 +0300

    gulp-jslint usage : burgerIcon.js is modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 var gulp = require('gulp');
 var csslint = require('gulp-csslint');
+var jslint = require('gulp-jslint');
 var concatCss = require('gulp-concat-css');
 var concat = require('gulp-concat');
 
 
-gulp.task('css', function() {
-  gulp.src('css/*.css')
+gulp.task('csslint', function() {
+  gulp.src('src/**/*.css')
     .pipe(csslint({
       'adjoining-classes':false
     }))
     .pipe(csslint.reporter());
+});
+
+gulp.task('jslint', function () {
+  return gulp.src(['src/js_files/*.js'])
+    .pipe(jslint({
+        node: true,
+        evil: true,
+        nomen: true,
+        global: [],
+        predef: [],
+        reporter: 'default',
+        edition: '2014-07-08',
+        errorsOnly: false
+    }))
+    .on('error', function (error) {
+            console.error(String(error));
+    });
 });
 
 gulp.task('concatcss', function () {
