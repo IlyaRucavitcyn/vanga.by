cca0585dd40c948b7176ab2893e088dba570ce6f
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-01-17 00:39:12 +0300

    Pausing set #1

--- js/all.js
+++ js/all.js
@@ -56,25 +56,22 @@ currencyChoiceContainer.onclick = function (e) {
     return;
 };
 
-// Hi there!
+'use strict';
+
+/*jslint browser:true */
+
 var vid = document.getElementById("bgvid"),
-pauseButton = document.getElementById("vidpause");
+    pauseButton = document.getElementById("vidpause");
 function vidFade() {
     vid.classList.add("stopfade");
 }
-vid.addEventListener('ended', function() {
-    // only functional if "loop" is removed
-     vid.pause();
-	// to capture IE10
-	vidFade();
-});
-pauseButton.addEventListener("click", function() {
+pauseButton.addEventListener("click", function () {
     vid.classList.toggle("stopfade");
-	if (vid.paused) {
-vid.play();
-		pauseButton.innerHTML = "Pause";
-	} else {
-        vid.pause();
-        pauseButton.innerHTML = "Paused";
-	}
-})
+    if (vid.paused) {
+        vid.play();
+        return false;
+    }
+    vid.pause();
+    return false;
+    }
+});
--- src/js_files/videoPause.js
+++ src/js_files/videoPause.js
@@ -7,19 +7,12 @@ var vid = document.getElementById("bgvid"),
 function vidFade() {
     vid.classList.add("stopfade");
 }
-vid.addEventListener('ended', function () {
-    // only functional if "loop" is removed
-    vid.pause();
-    // to capture IE10
-    vidFade();
-});
 pauseButton.addEventListener("click", function () {
     vid.classList.toggle("stopfade");
     if (vid.paused) {
         vid.play();
-        pauseButton.innerHTML = "Pause";
-    } else {
-        vid.pause();
-        pauseButton.innerHTML = "Paused";
+        return false;
     }
+    vid.pause();
+    return false;
 });
